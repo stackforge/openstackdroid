6ce2a6637ae2f7b07149b8dbaa2c3d0ae7279690
Ricardo Carrillo Cruz <ricardo.carrillo.cruz@gmail.com>
2013-04-16 11:30:39 +0200

    Add GetTokenOperation class
    This class represents a get token operation, it implements the command
    pattern

---
+++ com.rcarrillocruz.android.openstackdroid/src/com/rcarrillocruz/android/openstackdroid/GetTokenOperation.java
@@ -0,0 +1,48 @@
+package com.rcarrillocruz.android.openstackdroid;
+
+import java.io.UnsupportedEncodingException;
+
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.entity.StringEntity;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.google.gson.Gson;
+
+public class GetTokenOperation implements ApiOperation {
+	private Gson gson;
+	private static final String urlTail = "/v2.0/tokens";
+	
+	public GetTokenOperation() {
+		super();	
+		gson = new Gson();
+	}
+	
+	@Override
+	public HttpRequestBase invoke(Uri endpoint, String token, String tenantId,
+			Bundle params) {
+		// TODO Auto-generated method stub
+		HttpPost httpPost = new HttpPost(endpoint.toString()+urlTail);
+		httpPost.setHeader("Content-type", "application/json");
+		
+		String username = params.getString("username");
+		String password = params.getString("password");
+		tenantId = params.getString("tenantId");
+		
+		GetTokenRequest request = new GetTokenRequest(new AuthObject(new PasswordCredentialsObject(params.getString("username"), params.getString("password")), params.getString(tenantId)));
+		
+		StringEntity se = null;
+		try {
+			se = new StringEntity(gson.toJson(request));
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		httpPost.setEntity(se);
+		
+		return httpPost;
+	}
+}
