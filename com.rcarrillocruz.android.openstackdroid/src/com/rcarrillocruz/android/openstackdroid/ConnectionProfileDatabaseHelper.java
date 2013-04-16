21647c6b4abc50f9a0d2bbda10c0f649b2ba1cc2
Ricardo Carrillo Cruz <ricardo.carrillo.cruz@gmail.com>
2013-04-16 17:58:01 +0200

    Add ConnectionProfileDatabaseHelper class
    This is the SQLiteOpenHelper for interfacing with connection profiles

---
+++ com.rcarrillocruz.android.openstackdroid/src/com/rcarrillocruz/android/openstackdroid/ConnectionProfileDatabaseHelper.java
@@ -0,0 +1,36 @@
+package com.rcarrillocruz.android.openstackdroid;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class ConnectionProfileDatabaseHelper extends SQLiteOpenHelper {
+	private static final String DATABASE_NAME = "connection_profile.db";
+	private static final int DATABASE_VERSION = 1;
+	
+	
+	 public ConnectionProfileDatabaseHelper(Context context) {
+		 super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	 }
+	 
+	public ConnectionProfileDatabaseHelper(Context context, String name,
+			CursorFactory factory, int version) {
+		super(context, name, factory, version);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase database) {
+		// TODO Auto-generated method stub
+		ConnectionProfileTable.onCreate(database);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
