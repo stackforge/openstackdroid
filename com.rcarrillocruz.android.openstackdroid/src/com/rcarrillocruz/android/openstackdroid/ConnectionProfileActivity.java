e052000a2e188d2b4068ab2f94fc9a9f9cef5d51
Ricardo Carrillo Cruz <ricardo.carrillo.cruz@gmail.com>
2013-04-17 20:55:05 +0200

    Add ConnectionProfileActivity class
    This will handle creation of new connection profiles

---
+++ com.rcarrillocruz.android.openstackdroid/src/com/rcarrillocruz/android/openstackdroid/ConnectionProfileActivity.java
@@ -0,0 +1,15 @@
+package com.rcarrillocruz.android.openstackdroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ConnectionProfileActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.connection_profile_activity);
+	}
+
+}
