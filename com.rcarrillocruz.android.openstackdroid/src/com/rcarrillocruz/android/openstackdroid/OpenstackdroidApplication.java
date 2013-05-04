b10c89deb574aa9d1057ccac7beaca306c952f27
Ricardo Carrillo Cruz <ricardo.carrillo.cruz@gmail.com>
2013-05-04 13:26:58 +0200

    Add endpoints variables to reuse values among fragments

--- com.rcarrillocruz.android.openstackdroid/src/com/rcarrillocruz/android/openstackdroid/OpenstackdroidApplication.java
+++ com.rcarrillocruz.android.openstackdroid/src/com/rcarrillocruz/android/openstackdroid/OpenstackdroidApplication.java
@@ -4,6 +4,10 @@ import android.app.Application;
 
 public class OpenstackdroidApplication extends Application {
 	private String token;
+	private String computeEndpoint;
+	private String volumeEndpoint;
+	private String identityEndpoint;
+	private String imageEndpoint;
 	
 	@Override
 	public void onCreate() {
@@ -20,6 +24,30 @@ public class OpenstackdroidApplication extends Application {
 	}
 	public void setToken(String token) {
 		this.token = token;
+	}
+	public String getComputeEndpoint() {
+		return computeEndpoint;
+	}
+	public void setComputeEndpoint(String computeEndpoint) {
+		this.computeEndpoint = computeEndpoint;
+	}
+	public String getVolumeEndpoint() {
+		return volumeEndpoint;
+	}
+	public void setVolumeEndpoint(String volumeEndpoint) {
+		this.volumeEndpoint = volumeEndpoint;
+	}
+	public String getIdentityEndpoint() {
+		return identityEndpoint;
+	}
+	public void setIdentityEndpoint(String identityEndpoint) {
+		this.identityEndpoint = identityEndpoint;
+	}
+	public String getImageEndpoint() {
+		return imageEndpoint;
+	}
+	public void setImageEndpoint(String imageEndpoint) {
+		this.imageEndpoint = imageEndpoint;
 	}
 	
 }
